f74ca3c678beaac0e04b8e30c84be11554ae0eb3
Jodi De Loof <jodideloof@Jodis-MacBook-Pro.local>
2018-09-10 10:35:38 +0200

    stopping only our running image using startup.py script

--- startup.py
+++ startup.py
@@ -1,8 +1,9 @@
 import os
 
-os.system("docker-compose rm");
-#os.system("docker stop $(docker ps -a -q)")
-os.system("docker-compose up -d");
-os.system("docker-compose exec web python3 manage.py makemigrations");
-os.system("docker-compose exec web python3 manage.py migrate");
-os.system("docker-machine ls");
+os.system("docker-compose rm")
+os.system('docker rm $(docker stop $(docker ps -a -q --filter ancestor=vlaio-network-backend_web --format="{{.ID}}"))')
+os.system('docker rm $(docker stop $(docker ps -a -q --filter ancestor=postgres --format="{{.ID}}"))')
+os.system("docker-compose up -d")
+os.system("docker-compose exec web python3 manage.py makemigrations")
+os.system("docker-compose exec web python3 manage.py migrate")
+os.system("docker-machine ls")
