b8046b9b4f815cbbda03d0f5b2b1023d12c02579
azahmed096 <azahmed096@gmail.com>
2018-10-07 17:57:19 +0200

    decouple (for env vars), this closes #13

--- vlaio_prototype/settings.py
+++ vlaio_prototype/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,5 +150,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.domain.com'
 EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = 'no-reply@domain'
-EMAIL_HOST_PASSWORD = 'password' # Will be set on env var
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = config(
+    'FROM_EMAIL',
+    default='no-reply@domain'
+)
+
+EMAIL_HOST_PASSWORD = config(
+    'PASSOWRD_EMAIL',
+    default='password'
+)
