f43f69376bb88a8ad9503b88d3065efdee9d22a1
azahmed <azahmed096@gmail.com>
2018-10-14 19:45:04 +0200

    Update urls.py

--- vlaio_prototype/urls.py
+++ vlaio_prototype/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('api/user/', include(('authenticate.urls', 'authenticate'), namespace='auth'))
+    path('api/user/', include(('authenticate.urls', 'authenticate'), namespace='auth')),
     path('api/', include(('main.urls', 'main')))
 ]
