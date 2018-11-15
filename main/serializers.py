8ef8d6bb87ec00fb7663656ef73219fd853deb6c
azahmed096 <azahmed096@gmail.com>
2018-11-15 17:08:03 +0100

    interaction_set

--- main/serializers.py
+++ main/serializers.py
@@ -1,14 +1,30 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from .models import Company, Interaction,Partner,Overlap, DataFile
 
 
+class InteractionOfCompaySerializer(ModelSerializer):
+    partner = CharField(source='partner.name')
+
+    class Meta:
+        model = Interaction
+        fields = (
+            'id',
+            'partner',
+            'date',
+            'type'
+        )
+
+
 class CompanySerializer(ModelSerializer):
+    interaction_set = InteractionOfCompaySerializer(many=True)
+
     class Meta:
         model = Company
         fields = (
             'vat',
             'name',
+            'interaction_set',
             'employees',
             'profit',
         )
