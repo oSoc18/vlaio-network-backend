6c55021a61a755f6e7235678ec3dde0b7c4002ba
azahmed096 <azahmed096@gmail.com>
2018-11-24 17:15:56 +0100

    messages objects instead of strings

--- excel_parse/checkers.py
+++ excel_parse/checkers.py
@@ -6,15 +6,15 @@ if is_warning is False means that it is an error
 import pandas as pd
 from main.models import Company, Interaction, Partner
 from .vat_normilze import is_vat
+from . import upload_constants as const
+
 
 def check_new_parnter(df):
     partners_xl = df["Source"].unique()
-    print(df)
     partners_db = Partner.objects.values_list('name', flat=True).filter(name__in=partners_xl).distinct()
     partners_new = set(partners_xl) - set(partners_db)
-    print(partners_new)
     if len(partners_new):
-        return f"{len(partners_new)} partners will be added" + " ".join(partners_new), True
+        return {'message': const.MESSAGES.NEW_PARTNER, 'items': list(partners_new)}, True
 
 
 def check_new_types(df):
@@ -22,14 +22,14 @@ def check_new_types(df):
     types_db = Interaction.objects.values_list('type', flat=True).filter(type__in=types_xl).distinct()
     types_new = set(types_xl) - set(types_db)
     if len(types_new):
-        return f"{len(types_new)} types will be added to the database: " + ",".join(types_new), True
+        return {'message': const.MESSAGES.NEW_TYPES, 'items': list(types_new)}, True
 
 
 def check_empty_col(col_name):
     def checker(df):
         res = df.index[(df[col_name] == "") | (df[col_name] == "nan") | (df[col_name].isnull())] + 1
         if len(res):
-            return f"{len(res)} rows contains empty {col_name}:  among which {list(res[:3])}", False
+            return {'message': const.MESSAGES.EMPTY(col_name), 'items': list(res)}, False
     return checker
 
 
@@ -37,11 +37,10 @@ def check_new_vat(df):
     vat_news = get_new_vat(df)
 
     if vat_news:
-        return f"{len(vat_news)} vat are not in the database and will be added among which " + " ".join(vat_news[:3])  + "...", True
+        return {'message': const.MESSAGES.NEW_VATS, 'items': vat_news[:20]}, True
 
 def get_new_vat(df):
     vat_excl = set(df["VAT"].unique())
-    print(vat_excl)
     vat_db = set(Company.objects.filter(vat__in=vat_excl).values_list('vat', flat=True))
     vat_news = list(vat_excl - vat_db)
     return vat_news
@@ -56,4 +55,4 @@ def check_empty_type(df):
 def check_tva(df):
     res = df.index[df["VAT"].apply(lambda x: not is_vat(x))] + 1
     if len(res):
-        return f"{len(res)} rows contains bad VAT among which: {list(res[:3])}", False
+        return {'message': const.MESSAGES.BAD_VAT, 'items': list(res)}, False
---
+++ excel_parse/upload_constants.py
@@ -0,0 +1,6 @@
+class MESSAGES:
+    NEW_PARTNER = "News partners will be added"
+    NEW_TYPES = "News types will be added to the database"
+    NEW_VATS = "News VATs will be added"
+    EMPTY = "Column {0} does not allow empty or null values".format
+    BAD_VAT = "Bad vat's"
