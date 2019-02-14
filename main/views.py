d3ab73bd8746ac35aa37c3374083c6cd43b88158
jodiDL <jodi@jodideloof.be>
2019-02-14 15:54:50 +0100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -193,6 +193,10 @@ def apply_datafile(request, id):
     if filedata.applied:
         return Response({'error': 'already applied'}, status=401)
     
+    Company.objects.all().delete()
+    Partner.objects.all().delete()
+    Interaction.objects.all().delete()
+    
     data = INTERACTION_CONFIG.get_data_from_excel_file(
         os.path.join(settings.MEDIA_ROOT, filedata.file.name)
     )
